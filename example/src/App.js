643621b9f65f9b213c4739027cda3b2c474348f0
Markus Ort <ort@singhammer.com>
2021-11-28 00:11:16 +0100

    updated to function component

--- example/src/App.js
+++ example/src/App.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React from "react";
 
-import Cube from 'react-3d-cube';
+import Cube from "react-3d-cube";
 
-import front from './images/front.png';
-import right from './images/right.png';
-import back from './images/back.png';
-import left from './images/left.png';
-import top from './images/top.png';
-import bottom from './images/bottom.png';
+import front from "./images/front.png";
+import right from "./images/right.png";
+import back from "./images/back.png";
+import left from "./images/left.png";
+import top from "./images/top.png";
+import bottom from "./images/bottom.png";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>react-3d-cube</h1>
-        <h2>no children</h2>
-        <div
-          style={{
-            width: 300,
-            height: 300
-          }}
-        >
-          <Cube size={300} index="front" />
-        </div>
-        <h2>set children</h2>
-        <div
-          style={{
-            width: 300,
-            height: 300
-          }}
-        >
-          <Cube size={300} index="front">
-            <img src={front} alt="front" />
-            <img src={right} alt="right" />
-            <img src={back} alt="back" />
-            <img src={left} alt="left" />
-            <img src={top} alt="top" />
-            <img src={bottom} alt="bottom" />
-          </Cube>
-        </div>
+export default function App() {
+  return (
+    <div>
+      <h1>react-3d-cube</h1>
+      <h2>no children</h2>
+      <div
+        style={{
+          width: 300,
+          height: 300
+        }}
+      >
+        <Cube size={300} index="front" />
       </div>
-    );
-  }
+      <h2>set children</h2>
+      <div
+        style={{
+          width: 300,
+          height: 300
+        }}
+      >
+        <Cube size={300} index="front">
+          <img src={front} alt="front" />
+          <img src={right} alt="right" />
+          <img src={back} alt="back" />
+          <img src={left} alt="left" />
+          <img src={top} alt="top" />
+          <img src={bottom} alt="bottom" />
+        </Cube>
+      </div>
+    </div>
+  );
 }
--- src/cube.css.d.ts
+++ src/cube.css.d.ts
@@ -1,13 +1,26 @@
-declare namespace cube {
-	const cubeWrapper: string
-	const cube: string
-	const cubeFace: string
-	const empty: string
-	const front: string
-	const right: string
-	const back: string
-	const left: string
-	const top: string
-	const bottom: string
+export const cubeWrapper: string
+export const cube: string
+export const cubeFace: string
+export const empty: string
+export const front: string
+export const right: string
+export const back: string
+export const left: string
+export const top: string
+export const bottom: string
+interface Namespace {
+	"cubeWrapper": string,
+	"cube-wrapper": string,
+	"cube": string,
+	"cubeFace": string,
+	"cube-face": string,
+	"empty": string,
+	"front": string,
+	"right": string,
+	"back": string,
+	"left": string,
+	"top": string,
+	"bottom": string,
 }
+declare const cube: Namespace
 export default cube
