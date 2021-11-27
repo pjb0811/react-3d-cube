2c2acff0c2a305015ce57cdf7af1d4f5fec85610
Markus Ort <ort@singhammer.com>
2021-11-27 19:48:45 +0100

    created react hook component

--- src/Cube.tsx
+++ src/Cube.tsx
@@ -1,10 +1,14 @@
-import * as React from 'react';
-import { Motion, spring } from 'react-motion';
-import styles from './cube.css';
+import * as React from "react";
+import { FunctionComponent, useEffect, useState } from "react";
+import { Motion, spring } from "react-motion";
+import styles from "./cube.css";
 
 const emptyFace = <div className={styles.empty}>empty</div>;
 
-type Props = { index: string; size: number };
+type Props = {
+  index: "front" | "right" | "back" | "left" | "top" | "bottom";
+  size: number;
+};
 
 type State = {
   index: string;
@@ -29,119 +33,117 @@ type State = {
   children: Array<React.ReactNode>;
 };
 
-class Cube extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
+export const Cube: FunctionComponent<Props> = ({
+  index = "front",
+  size = 200,
+  children
+}) => {
+  const faceSize = size / 2;
 
-    const { index = 'front', size = 200 } = this.props;
-    const faceSize = size / 2;
-
-    this.state = {
-      index,
-      isMoved: false,
-      isPressed: false,
-      mouseXY: [0, 0],
-      mouseDelta: [0, 0],
-      distance: 0,
-      cube: {
-        width: size,
-        height: size,
+  const [state, setState] = useState<State>({
+    index,
+    isMoved: false,
+    isPressed: false,
+    mouseXY: [0, 0],
+    mouseDelta: [0, 0],
+    distance: 0,
+    cube: {
+      width: size,
+      height: size,
+      translate: {
+        x: 0,
+        y: 0,
+        z: -faceSize
+      },
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0
+    },
+    faces: {
+      front: {
+        translate: {
+          x: 0,
+          y: 0,
+          z: faceSize
+        },
+        rotate: {
+          x: 0,
+          y: 1,
+          z: 0,
+          deg: 0
+        }
+      },
+      right: {
+        translate: {
+          x: faceSize,
+          y: 0,
+          z: 0
+        },
+        rotate: {
+          x: 0,
+          y: 1,
+          z: 0,
+          deg: 90
+        }
+      },
+      back: {
         translate: {
           x: 0,
           y: 0,
           z: -faceSize
         },
-        rotateX: 0,
-        rotateY: 0,
-        rotateZ: 0
-      },
-      faces: {
-        front: {
-          translate: {
-            x: 0,
-            y: 0,
-            z: faceSize
-          },
-          rotate: {
-            x: 0,
-            y: 1,
-            z: 0,
-            deg: 0
-          }
-        },
-        right: {
-          translate: {
-            x: faceSize,
-            y: 0,
-            z: 0
-          },
-          rotate: {
-            x: 0,
-            y: 1,
-            z: 0,
-            deg: 90
-          }
-        },
-        back: {
-          translate: {
-            x: 0,
-            y: 0,
-            z: -faceSize
-          },
-          rotate: {
-            x: 0,
-            y: 1,
-            z: 0,
-            deg: 180
-          }
-        },
-        left: {
-          translate: {
-            x: -faceSize,
-            y: 0,
-            z: 0
-          },
-          rotate: {
-            x: 0,
-            y: 1,
-            z: 0,
-            deg: -90
-          }
-        },
-        top: {
-          translate: {
-            x: 0,
-            y: -faceSize,
-            z: 0
-          },
-          rotate: {
-            x: 1,
-            y: 0,
-            z: 0,
-            deg: 90
-          }
-        },
-        bottom: {
-          translate: {
-            x: 0,
-            y: faceSize,
-            z: 0
-          },
-          rotate: {
-            x: 1,
-            y: 0,
-            z: 0,
-            deg: -90
-          }
+        rotate: {
+          x: 0,
+          y: 1,
+          z: 0,
+          deg: 180
         }
       },
-      children: []
-    };
-  }
+      left: {
+        translate: {
+          x: -faceSize,
+          y: 0,
+          z: 0
+        },
+        rotate: {
+          x: 0,
+          y: 1,
+          z: 0,
+          deg: -90
+        }
+      },
+      top: {
+        translate: {
+          x: 0,
+          y: -faceSize,
+          z: 0
+        },
+        rotate: {
+          x: 1,
+          y: 0,
+          z: 0,
+          deg: 90
+        }
+      },
+      bottom: {
+        translate: {
+          x: 0,
+          y: faceSize,
+          z: 0
+        },
+        rotate: {
+          x: 1,
+          y: 0,
+          z: 0,
+          deg: -90
+        }
+      }
+    },
+    children: []
+  });
 
-  componentDidMount() {
-    const { children } = this.props;
-    const { index, cube, faces } = this.state;
+  useEffect(() => {
+    const { index, cube, faces } = state;
     const newChildren = children
       ? Array.isArray(children)
         ? [...children].slice(0, 6)
@@ -152,7 +154,8 @@ class Cube extends React.Component<Props, State> {
       newChildren.push(emptyFace);
     }
 
-    this.setState({
+    setState({
+      ...state,
       cube: {
         ...cube,
         rotateX: faces[index].rotate.x ? -faces[index].rotate.deg : 0,
@@ -161,10 +164,10 @@ class Cube extends React.Component<Props, State> {
       },
       children: newChildren
     });
-  }
+  }, []);
 
-  dragCube = () => {
-    const { cube, mouseDelta } = this.state;
+  const dragCube = () => {
+    const { cube, mouseDelta } = state;
     const [dx, dy] = mouseDelta;
 
     const rotateX = cube.rotateX - dy;
@@ -172,7 +175,8 @@ class Cube extends React.Component<Props, State> {
     const rotateY =
       absX > 90 && absX < 270 ? cube.rotateY - dx : cube.rotateY + dx;
 
-    this.setState({
+    setState({
+      ...state,
       cube: {
         ...cube,
         rotateX,
@@ -181,10 +185,11 @@ class Cube extends React.Component<Props, State> {
     });
   };
 
-  handleMouseDown = (e: React.MouseEvent) => {
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const { pageX, pageY } = e;
 
-    this.setState({
+    setState({
+      ...state,
       isPressed: true,
       isMoved: false,
       mouseXY: [pageX, pageY]
@@ -193,29 +198,31 @@ class Cube extends React.Component<Props, State> {
     e.preventDefault();
   };
 
-  handleMouseMove = (e: { pageX: number; pageY: number }) => {
+  const handleMouseMove = (e: { pageX: number; pageY: number }) => {
     const { pageX, pageY } = e;
     const {
       isPressed,
       mouseXY: [mx, my]
-    } = this.state;
+    } = state;
 
     if (isPressed) {
-      this.setState({
+      setState({
+        ...state,
         mouseXY: [pageX, pageY],
         mouseDelta: [pageX - mx, pageY - my],
         isMoved: true
       });
 
-      this.dragCube();
+      dragCube();
     }
   };
 
-  handleMouseUp = () => {
-    const { isPressed } = this.state;
+  const handleMouseUp = () => {
+    const { isPressed } = state;
 
     if (isPressed) {
-      this.setState({
+      setState({
+        ...state,
         isPressed: false,
         mouseXY: [0, 0],
         mouseDelta: [0, 0]
@@ -223,69 +230,58 @@ class Cube extends React.Component<Props, State> {
     }
   };
 
-  render() {
-    const { cube, faces, children } = this.state;
-    return (
-      <div className={styles.cubeWrapper}>
-        <Motion
-          style={{
-            rotateX: spring(cube.rotateX),
-            rotateY: spring(cube.rotateY),
-            rotateZ: spring(cube.rotateZ)
-          }}
-        >
-          {({ rotateX, rotateY, rotateZ }) => {
-            return (
-              <div
-                className={styles.cube}
-                style={{
-                  transform: `translate3d(${cube.translate.x}px, ${
-                    cube.translate.y
-                  }px, ${
-                    cube.translate.z
-                  }px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`
-                }}
-              >
-                {Object.keys(faces).map((face, key) => {
-                  const { translate, rotate } = faces[face];
+  return (
+    <div className={styles.cubeWrapper}>
+      <Motion
+        style={{
+          rotateX: spring(state.cube.rotateX),
+          rotateY: spring(state.cube.rotateY),
+          rotateZ: spring(state.cube.rotateZ)
+        }}
+      >
+        {({ rotateX, rotateY, rotateZ }) => {
+          return (
+            <div
+              className={styles.cube}
+              style={{
+                transform: `translate3d(${state.cube.translate.x}px, ${state.cube.translate.y}px, ${state.cube.translate.z}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`
+              }}
+            >
+              {Object.keys(state.faces).map((face, key) => {
+                const { translate, rotate } = state.faces[face];
 
-                  return (
-                    <div
-                      className={`${styles.cubeFace} ${styles[face]}`}
-                      key={key}
-                      style={{
-                        transform: `translate3d(${translate.x}px, ${
-                          translate.y
-                        }px, ${translate.z}px) rotate3d(${rotate.x}, ${
-                          rotate.y
-                        }, ${rotate.z}, ${rotate.deg}deg)`,
-                        width: cube.width,
-                        height: cube.height
-                      }}
-                      onMouseDown={e => {
-                        this.handleMouseDown(e);
-                      }}
-                      onMouseMove={e => {
-                        this.handleMouseMove(e);
-                      }}
-                      onMouseLeave={() => {
-                        this.handleMouseUp();
-                      }}
-                      onMouseUp={() => {
-                        this.handleMouseUp();
-                      }}
-                    >
-                      {children[key]}
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          }}
-        </Motion>
-      </div>
-    );
-  }
-}
-
-export default Cube;
+                return (
+                  <div
+                    className={`${styles.cubeFace} ${styles[face]}`}
+                    key={key}
+                    style={{
+                      transform: `translate3d(${translate.x}px, ${translate.y}px, ${translate.z}px) rotate3d(${rotate.x}, ${rotate.y}, ${rotate.z}, ${rotate.deg}deg)`,
+                      width: state.cube.width,
+                      height: state.cube.height
+                    }}
+                    onMouseDown={e => {
+                      handleMouseDown(
+                        e as React.MouseEvent<HTMLDivElement, MouseEvent>
+                      );
+                    }}
+                    onMouseMove={e => {
+                      handleMouseMove(e);
+                    }}
+                    onMouseLeave={() => {
+                      handleMouseUp();
+                    }}
+                    onMouseUp={() => {
+                      handleMouseUp();
+                    }}
+                  >
+                    {children && children[key]}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        }}
+      </Motion>
+    </div>
+  );
+};
